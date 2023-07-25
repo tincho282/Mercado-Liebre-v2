93a8ec7c567dac385a444df4bfa173dac6ba77dd
tincho282 <tincho282@hotmail.com>
2023-07-25 20:03:58 -0300

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.listen(3000, ()=> console.log('Servidor funcionando en: http://localhost:3000'));
+/*app.listen(3000, ()=> console.log('Servidor funcionando en: http://localhost:3000'));*/
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
