68582f7d5609e680f2feb1a131ec88dfb4c1b28a
tincho282 <tincho282@hotmail.com>
2023-07-25 20:06:03 -0300

    app.js

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.listen(process.env.PORT || 3000, function() {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
