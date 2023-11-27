22896980f3c600ef3935839c4e17209271217a0a
Martin Gomez <tincho282@hotmail.com>
2023-11-27 14:51:18 -0300

    Solucion de fallos y vistas del CSS

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-/*app.listen(3000, ()=> console.log('Servidor funcionando en: http://localhost:3000'));*/
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
-  })
+app.listen(process.env.PORT || 3010, function() {
+    console.log("Servidor corriendo: http://localhost:3010");
+})
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-})
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'));
-})
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'));
-})
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
